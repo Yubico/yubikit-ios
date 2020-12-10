6a4a21d5a1d3da7fe48b012df8da681fbe186d94
Jens Utbult <jens.utbult@yubico.com>
2020-12-10 16:13:53 +0100

    Rudimentary documentation of YubiKitManager.

--- YubiKit/YubiKit/YubiKitManager.h
+++ YubiKit/YubiKit/YubiKitManager.h
@@ -18,34 +18,131 @@
 #import "YKFNFCConnection.h"
 #import "YKFAccessoryConnection.h"
 
+
 /*!
- @protocol YubiKitManagerProtocol
+ @protocol YKFManagerDelegate
  
  @abstract
-    Provides the main access point interface for YubiKit.
+   Implement this protocol to get notifications when a connection to the YubiKey is established or broken.
  */
-
 @protocol YKFManagerDelegate <NSObject>
 
+/*!
+ @method didConnectNFC:connection
+ 
+ @abstract
+    The YubiKey SDK did connect to a NFC Yubikey.
+ 
+ @param connection
+    The YKFNFCConnection to the YubiKey.
+ */
 - (void)didConnectNFC:(YKFNFCConnection *_Nonnull)connection;
+
+/*!
+ @method didDisconnectNFC:connection:error
+ 
+ @abstract
+    The YubiKey SDK did connect to a NFC Yubikey.
+
+ @param connection
+    The YKFNFCConnection to the YubiKey that did disconnect.
+ 
+ @method error
+    If the disconnection was unexpted an NSError will be passed.
+  */
 - (void)didDisconnectNFC:(YKFNFCConnection *_Nonnull)connection error:(NSError *_Nullable)error;
 
+/*!
+ @method didConnectAccessory:connection
+ 
+ @abstract
+    The YubiKey SDK did connect to a Accessory Yubikey.
+ 
+ @param connection
+    The YKFAccessoryConnection to the YubiKey.
+ */
 - (void)didConnectAccessory:(YKFAccessoryConnection *_Nonnull)connection;
+
+/*!
+ @method didDisconnectAccessory:connection:error
+ 
+ @abstract
+    The YubiKey SDK did connect to a Accessory Yubikey.
+
+ @param connection
+    The YKFAccessoryConnection to the YubiKey that did disconnect.
+ 
+ @method error
+    If the disconnection was unexpted an NSError will be passed.
+  */
 - (void)didDisconnectAccessory:(YKFAccessoryConnection *_Nonnull)connection error:(NSError *_Nullable)error;
 
 @end
 
 
+/*!
+ @interface YubiKitManager
+ 
+ @abstract
+    Provides the main access point interface for YubiKit.
+ */
 @interface YubiKitManager : NSObject
 
+/*!
+ @property delegate
+ 
+ @abstract
+    The delegate must conform to the YKFManagerDelegate protocol. Setting this delegate will allow you
+    to get notifications when a connection to the YubiKey is established or broken.
+ */
 @property(nonatomic, weak) id<YKFManagerDelegate> _Nullable delegate;
 
+/*!
+ @method startNFCConnection
+ 
+ @abstract
+    Start the NFC connection.
+ */
 - (void)startNFCConnection API_AVAILABLE(ios(13.0));
+
+/*!
+ @method stopNFCConnection
+ 
+ @abstract
+    Stop the NFC connection.
+ 
+ @discussion
+    This is typically done as soon as you have finished your operations on the YubiKey. Stopping the NFC connection
+    will also dismiss the NFC system modal presented by iOS during NFC operations.
+ */
 - (void)stopNFCConnection API_AVAILABLE(ios(13.0));
 
+/*!
+ @method startAccessoryConnection
+ 
+ @abstract
+    Start the accessory connection.
+ 
+ @discussion
+    Do this when the application becomes active to continuosly listen for a YubiKey inserted into the Lightning Port.
+ */
 - (void)startAccessoryConnection;
+
+/*!
+ @method stopAccessoryConnection
+ 
+ @abstract
+    Stop the accessory connection.
+ */
 - (void)stopAccessoryConnection;
 
+
+/*!
+ @property otpSession
+ 
+ @abstract
+    The YKFNFCOTPSession.
+ */
 @property(nonatomic, nonnull, readonly) YKFNFCOTPSession *otpSession API_AVAILABLE(ios(11.0));
 
 /*!
