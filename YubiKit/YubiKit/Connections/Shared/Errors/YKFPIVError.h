d33aab4ab69f31488fba7783eac59393e873368a
Jens Utbult <jens.utbult@yubico.com>
2021-02-26 15:18:42 +0100

    PIV authenticate.

---
+++ YubiKit/YubiKit/Connections/Shared/Errors/YKFPIVError.h
@@ -0,0 +1,13 @@
+//
+//  YKFPIVError.h
+//  YubiKit
+//
+//  Created by Jens Utbult on 2021-02-25.
+//  Copyright © 2021 Yubico. All rights reserved.
+//
+
+#ifndef YKFPIVError_h
+#define YKFPIVError_h
+
+
+#endif /* YKFPIVError_h */
---
+++ YubiKit/YubiKit/Connections/Shared/Errors/YKFPIVError.m
@@ -0,0 +1,9 @@
+//
+//  YKFPIVError.m
+//  YubiKit
+//
+//  Created by Jens Utbult on 2021-02-25.
+//  Copyright © 2021 Yubico. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
