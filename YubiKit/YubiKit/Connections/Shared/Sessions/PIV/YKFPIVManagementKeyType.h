9573525f67a7e41602a84f485ca94d6b683fcb97
Jens Utbult <jens.utbult@yubico.com>
2021-02-26 15:18:42 +0100

    PIV authenticate.

---
+++ YubiKit/YubiKit/Connections/Shared/Sessions/PIV/YKFPIVManagementKeyType.h
@@ -0,0 +1,13 @@
+//
+//  YKFPIVManagementKeyType.h
+//  YubiKit
+//
+//  Created by Jens Utbult on 2021-02-23.
+//  Copyright © 2021 Yubico. All rights reserved.
+//
+
+#ifndef YKFPIVManagementKeyType_h
+#define YKFPIVManagementKeyType_h
+
+
+#endif /* YKFPIVManagementKeyType_h */
---
+++ YubiKit/YubiKit/Connections/Shared/Sessions/PIV/YKFPIVManagementKeyType.m
@@ -0,0 +1,9 @@
+//
+//  YKFManagementKeyType.m
+//  YubiKit
+//
+//  Created by Jens Utbult on 2021-02-23.
+//  Copyright © 2021 Yubico. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
