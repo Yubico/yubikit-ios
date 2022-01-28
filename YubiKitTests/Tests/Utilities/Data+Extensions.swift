84f85f27dfb4fee2f6b61463b38dcb9438184ff6
Jens Utbult <jens.utbult@yubico.com>
2022-01-28 10:45:19 +0100

    Fixes bugs in pin handling for FIDO2. Increased test coverage for FIDO2.

--- YubiKitTests/Tests/Utilities/Data+Extensions.swift
+++ YubiKitTests/Tests/Utilities/Data+Extensions.swift
@@ -18,4 +18,10 @@ extension Data {
     var hexDescription: String {
         return reduce("") {$0 + String(format: "%02x", $1)}
     }
+    
+    var bytes: [UInt8] {
+        var byteArray = [UInt8](repeating: 0, count: self.count)
+        self.copyBytes(to: &byteArray, count: self.count)
+        return byteArray
+    }
 }
