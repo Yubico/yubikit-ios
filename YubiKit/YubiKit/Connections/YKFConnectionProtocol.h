dc6ba96b63e16d434562e07ae6c4d3885944e2ef
Jens Utbult <jens.utbult@gmail.com>
2025-03-21 13:08:35 +0100

    Session creation documentation for SCP.

--- YubiKit/YubiKit/Connections/YKFConnectionProtocol.h
+++ YubiKit/YubiKit/Connections/YKFConnectionProtocol.h
@@ -26,6 +26,11 @@ typedef void (^YKFOATHSessionCompletionBlock)(YKFOATHSession *_Nullable, NSError
 /// @param completion The completion handler that gets called once the application is selected on
 ///                   the YubiKey. This handler is executed on a background thread.
 - (void)oathSession:(YKFOATHSessionCompletionBlock _Nonnull)completion;
+
+/// @abstract Returns a YKFOATHSession for interacting with the OATH application on the YubiKey.
+/// @param scpKeyParams SCP key params for the session.
+/// @param completion The completion handler that gets called once the application is selected on
+///                   the YubiKey. This handler is executed on a background thread.
 - (void)oathSession:(id<YKFSCPKeyParamsProtocol> _Nonnull)scpKeyParams completion:(YKFOATHSessionCompletionBlock _Nonnull)completion;
 
 typedef void (^YKFU2FSessionCompletionBlock)(YKFU2FSession *_Nullable, NSError* _Nullable);
@@ -34,6 +39,11 @@ typedef void (^YKFU2FSessionCompletionBlock)(YKFU2FSession *_Nullable, NSError* 
 /// @param completion The completion handler that gets called once the application is selected on
 ///                   the YubiKey. This handler is executed on a background thread.
 - (void)u2fSession:(YKFU2FSessionCompletionBlock _Nonnull)completion;
+
+/// @abstract Returns a YKFU2FSession for interacting with the U2F application on the YubiKey.
+/// @param scpKeyParams SCP key params for the session.
+/// @param completion The completion handler that gets called once the application is selected on
+///                   the YubiKey. This handler is executed on a background thread.
 - (void)u2fSession:(id<YKFSCPKeyParamsProtocol> _Nonnull)scpKeyParams completion:(YKFU2FSessionCompletionBlock _Nonnull)completion;
 
 typedef void (^YKFFIDO2SessionCompletionBlock)(YKFFIDO2Session *_Nullable, NSError* _Nullable);
@@ -43,12 +53,23 @@ typedef void (^YKFFIDO2SessionCompletionBlock)(YKFFIDO2Session *_Nullable, NSErr
 ///                   the YubiKey. This handler is executed on a background thread.
 - (void)fido2Session:(YKFFIDO2SessionCompletionBlock _Nonnull)completion;
 
+/// @abstract Returns a YKFFIDO2Session for interacting with the FIDO2 application on the YubiKey.
+/// @param scpKeyParams SCP key params for the session.
+/// @param completion The completion handler that gets called once the application is selected on
+///                   the YubiKey. This handler is executed on a background thread.
+- (void)fido2Session:(id<YKFSCPKeyParamsProtocol> _Nonnull)scpKeyParams completion:(YKFFIDO2SessionCompletionBlock _Nonnull)completion;
+
 typedef void (^YKFPIVSessionCompletionBlock)(YKFPIVSession *_Nullable, NSError* _Nullable);
 
 /// @abstract Returns a YKFPIVSession for interacting with the PIV application on the YubiKey.
 /// @param completion The completion handler that gets called once the application is selected on
 ///                   the YubiKey. This handler is executed on a background thread.
 - (void)pivSession:(YKFPIVSessionCompletionBlock _Nonnull)completion;
+
+/// @abstract Returns a YKFPIVSession for interacting with the PIV application on the YubiKey.
+/// @param scpKeyParams SCP key params for the session.
+/// @param completion The completion handler that gets called once the application is selected on
+///                   the YubiKey. This handler is executed on a background thread.
 - (void)pivSession:(id<YKFSCPKeyParamsProtocol> _Nonnull)scpKeyParams completion:(YKFPIVSessionCompletionBlock _Nonnull)completion;
 
 typedef void (^YKFChallengeResponseSessionCompletionBlock)(YKFChallengeResponseSession *_Nullable, NSError* _Nullable);
@@ -57,6 +78,11 @@ typedef void (^YKFChallengeResponseSessionCompletionBlock)(YKFChallengeResponseS
 /// @param completion The completion handler that gets called once the application is selected on
 ///                   the YubiKey. This handler is executed on a background thread.
 - (void)challengeResponseSession:(YKFChallengeResponseSessionCompletionBlock _Nonnull)completion;
+
+/// @abstract Returns a YKFChallengeResponseSession for interacting with the Challenge Response application on the YubiKey.
+/// @param scpKeyParams SCP key params for the session.
+/// @param completion The completion handler that gets called once the application is selected on
+///                   the YubiKey. This handler is executed on a background thread.
 - (void)challengeResponseSession:(id<YKFSCPKeyParamsProtocol> _Nonnull)scpKeyParams completion:(YKFChallengeResponseSessionCompletionBlock _Nonnull)completion;
 
 typedef void (^YKFManagementSessionCompletion)(YKFManagementSession *_Nullable, NSError* _Nullable);
@@ -65,11 +91,21 @@ typedef void (^YKFManagementSessionCompletion)(YKFManagementSession *_Nullable, 
 /// @param completion The completion handler that gets called once the application is selected on
 ///                   the YubiKey. This handler is executed on a background thread.
 - (void)managementSession:(YKFManagementSessionCompletion _Nonnull)completion;
+
+/// @abstract Returns a YKFManagementSession for interacting with the Management application on the YubiKey.
+/// @param scpKeyParams SCP key params for the session.
+/// @param completion The completion handler that gets called once the application is selected on
+///                   the YubiKey. This handler is executed on a background thread.
 - (void)managementSession:(id<YKFSCPKeyParamsProtocol> _Nonnull)scpKeyParams completion:(YKFManagementSessionCompletion _Nonnull)completion;
 
 typedef void (^YKFSecurityDomainSessionCompletion)(YKFSecurityDomainSession *_Nullable, NSError* _Nullable);
 
 - (void)securityDomainSession:(YKFSecurityDomainSessionCompletion _Nonnull)completion;
+
+/// @abstract Returns a YKFSecurityDomainSession for interacting with the Security Domain application on the YubiKey.
+/// @param scpKeyParams SCP key params for the session.
+/// @param completion The completion handler that gets called once the application is selected on
+///                   the YubiKey. This handler is executed on a background thread.
 - (void)securityDomainSession:(id<YKFSCPKeyParamsProtocol> _Nonnull)scpKeyParams completion:(YKFSecurityDomainSessionCompletion _Nonnull)completion;
 
 /// @abstract The smart card interface to the YubiKey.
